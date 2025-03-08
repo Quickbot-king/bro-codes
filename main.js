aa66090773b03389e9aca90f5303111de2809f2b
King Kong <kevinmckenzie030@gmail.com>
2025-03-08 12:22:50 +0300

    This is the counter problem solved

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-const decreaseBtn=document.getElementById("decreaseBtn");
+const myLabel=document.getElementById("myLabel");
+const increaseBtn=document.getElementById("increaseBtn");
 const resetBtn=document.getElementById("resetBtn");
-const increaseeBtn=document.getElementById("increaseBtn");
-const countLabel=document.getElementById("countLabel");
+const decreaseBtn=document.getElementById("decreaseBtn");
+
 let count=0;
 
-increaseBtn.onclick=function(){
-    count++
-    countLabel.textContent=count;
-}
-decreaseBtn.onclick=function(){
-    count--;
-    countLabel.textContent=count;
+increaseBtn.onclick= function(){
+     count++;
+     myLabel.textContent=count;
 }
 resetBtn.onclick=function(){
     count=0;
-    countLabel.textContent=count;
+    myLabel.textContent=count;
+}
+decreaseBtn.onclick=function(){
+    count--;
+    myLabel.textContent=count;
+
 }
