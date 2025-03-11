00eaa3dcaaa4bf497321e13e84c886bb9973df78
King Kong <kevinmckenzie030@gmail.com>
2025-03-11 12:34:57 +0300

    randomNumber generator tutorial complete

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-const myLabel=document.getElementById("myLabel");
-const increaseBtn=document.getElementById("increaseBtn");
-const resetBtn=document.getElementById("resetBtn");
-const decreaseBtn=document.getElementById("decreaseBtn");
-
-let count=0;
-
-increaseBtn.onclick= function(){
-     count++;
-     myLabel.textContent=count;
-}
-resetBtn.onclick=function(){
-    count=0;
-    myLabel.textContent=count;
-}
-decreaseBtn.onclick=function(){
-    count--;
-    myLabel.textContent=count;
-
-}
+let randomNumber=Math.floor(Math.random()*6)+1;
+console.log(randomNumber);
---
+++ randomNumber.js
@@ -0,0 +1,23 @@
+const min=1;
+const max=6;
+const myButton=document.getElementById("myButton");
+const myLabel1=document.getElementById("myLabel1");
+const myLabel2=document.getElementById("myLabel2");
+const myLabel3=document.getElementById("myLabel3");
+const myLabel4=document.getElementById("myLabel4");
+
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
+let randomNumber4;
+
+myButton.onclick=function(){
+    randomNumber1=Math.floor(Math.random()*max)+min;
+    randomNumber2=Math.floor(Math.random()*max)+min;
+    randomNumber3=Math.floor(Math.random()*max)+min;
+    randomNumber4=Math.floor(Math.random()*max)+min;
+    myLabel1.textContent=randomNumber1;
+    myLabel2.textContent=randomNumber2;
+    myLabel3.textContent=randomNumber3;
+    myLabel4.textContent=randomNumber4;
+}
