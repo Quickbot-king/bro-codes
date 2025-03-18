860ae1ace1834b9c8061fbf2e7d2886880fe4a87
King Kong <kevinmckenzie030@gmail.com>
2025-03-18 18:02:58 +0300

    18th march 2025

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-let randomNumber=Math.floor(Math.random()*6)+1;
-console.log(randomNumber);
+const myText=document.getElementById("myText");
+const mySubmit=document.getElementById("mySubmit");
+const resultElement=document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick=function(){
+    age=myText.value;
+    age=Number(age)
+if (age>=100){
+    resultElement.textContent=`You are too old to do this ,sir`;
+}else if(age==0){
+    resultElement.textContent=`You can't enter,you was just born`;
+}else if(age>=18){
+    resultElement.textContent=`you are old enough to drive`;
+}else if(age<0){
+    resultElement.textContent=`You can't have a negative age and want to drive`;
+}else{
+    resultElement.textContent=`You must be 18+ to enter this site`;
+
+}}
--- quiz.py
+++
@@ -1,3 +0,0 @@
-my_num=20
-my_num+=20
-print(my_num)
